d30956dfeabafb5fbc2ad4cc0b48674c8c13bce6
Davis <brandoncq036@hotmail.com>
2024-11-04 21:45:36 -0500

    agregando bg-2

--- src/app/Soporte/client.jsx
+++ src/app/Soporte/client.jsx
@@ -9,7 +9,7 @@ export default function SoporteClient() {
             Centro de Soporte al Cliente
           </h2>
           <img
-            src="https://images.unsplash.com/photo-1547483238-2cbf881a559f?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            src="https://plus.unsplash.com/premium_photo-1712736395906-dd23bfe7f3b2?q=80&w=2075&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
             alt=""
             className="w-full h-full object-cover"
           />
