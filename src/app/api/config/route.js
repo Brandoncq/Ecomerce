ce5fc32b9e90bb409f79d040da0960555c3e42a4
Davis <brandoncq036@hotmail.com>
2025-01-01 14:02:50 -0500

    cambiando base de datos

--- src/app/api/config/route.js
+++ src/app/api/config/route.js
@@ -1,5 +1,19 @@
 import mysql from "mysql2/promise";
 
+/*const isLocal = process.env.NODE_ENV !== "production";
+
+const pool = mysql.createPool({
+  host: isLocal ? "localhost" : process.env.DB_HOST,
+  user: isLocal ? "root" : process.env.DB_USER,
+  database: isLocal ? "ecomerce" : process.env.DB_DATABASE,
+  password: isLocal ? "" : process.env.DB_PASSWORD,
+  port: isLocal ? 3306 : process.env.DB_PORT,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+  timezone: "SYSTEM",
+});*/
+
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
